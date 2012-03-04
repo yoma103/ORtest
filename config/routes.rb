29530d10031354ee9cc68d3d5d5b0cd167b2b8f8
Denis Kovalsky <kovalsky.d.s@gmail.com>
2012-03-04 16:38:06 +0300

    finished layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 ORtest::Application.routes.draw do
-  get "pages/home"
+  get "users/new"
 
-  get "pages/contact"
-    
-    get "pages/about"
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  match '/signup', :to => 'users#new'
+
+  root :to => 'pages#home'
 
   resources :people
 
